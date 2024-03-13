4d9d56c85f97cdf4a7ef544cc2767ec75629541a
Хусан С. Абетов <abetov.x@ad.loc>
2024-03-13 15:49:38 +0500

    FEATURE: Add Asad changes

--- pages/profile/about.vue
+++ pages/profile/about.vue
@@ -1,85 +1,109 @@
 <template>
-    <div class="about">
-      <UiHeaderComponent
-        center
-        center-text="О проекте"
-        left-route="/profile"
-      ></UiHeaderComponent>
-      <div class="about-wrapper">
-        <div class="about-info">
-            <div class="about-logo">
-                <NuxtImg src="/images/logo.svg"></NuxtImg>
-            </div>
-            <div class="about-text">
-                <p>Мы соединяем тех, кто может помочь, с теми, кому нужна помощь. Мы создаем простой способ участия в благотворительности для пользователей Click и возможность регулярно получать помощь для некоммерческих организаций.</p>
-                <p>Фонд «Inson Uchun» был создан в 2005-м году по инициативе еженедельной газеты «Аргументы и факты». За эти годы больше 10 тысяч подопечных со сложными диагнозами получили помощь с оплатой операций, лекарств и медоборудования. Мы помогаем тяжелобольным детям и взрослым из разных регионов Узбекистана, а также развиваем проекты по всесторонней поддержке подопечных семей — чтобы никто не оставался наедине с болезнью.</p>
-            </div>
+  <div class="about">
+    <UiHeaderComponent
+      center
+      center-text="О проекте"
+      left-route="/profile"
+    ></UiHeaderComponent>
+    <div class="about-wrapper">
+      <div class="about-info">
+        <div class="about-logo">
+          <NuxtImg src="/images/logo.svg"></NuxtImg>
         </div>
-        <div class="about-soc">
-            <a href="https://t.me/clickuz" target="_blank" class="about-soc-item"><NuxtImg src="/images/telegram.svg"></NuxtImg></a>
-            <a href="https://www.instagram.com/clickuz/" target="_blank" class="about-soc-item"><NuxtImg src="/images/instagram.svg"></NuxtImg></a>
-            <a href="https://www.youtube.com/channel/UCC9YNXA4HSbk34fhXpFMyLw" target="_blank" class="about-soc-item"><NuxtImg src="/images/youtube.svg"></NuxtImg></a>
+        <div class="about-text">
+          <p>
+            Мы соединяем тех, кто может помочь, с теми, кому нужна помощь. Мы
+            создаем простой способ участия в благотворительности для
+            пользователей Click и возможность регулярно получать помощь для
+            некоммерческих организаций.
+          </p>
+          <p>
+            Фонд «Inson Uchun» был создан в 2005-м году по инициативе
+            еженедельной газеты «Аргументы и факты». За эти годы больше 10 тысяч
+            подопечных со сложными диагнозами получили помощь с оплатой
+            операций, лекарств и медоборудования. Мы помогаем тяжелобольным
+            детям и взрослым из разных регионов Узбекистана, а также развиваем
+            проекты по всесторонней поддержке подопечных семей — чтобы никто не
+            оставался наедине с болезнью.
+          </p>
         </div>
       </div>
+      <div class="about-soc">
+        <a href="https://t.me/clickuz" target="_blank" class="about-soc-item"
+          ><NuxtImg src="/images/telegram.svg"></NuxtImg
+        ></a>
+        <a
+          href="https://www.instagram.com/clickuz/"
+          target="_blank"
+          class="about-soc-item"
+          ><NuxtImg src="/images/instagram.svg"></NuxtImg
+        ></a>
+        <a
+          href="https://www.youtube.com/channel/UCC9YNXA4HSbk34fhXpFMyLw"
+          target="_blank"
+          class="about-soc-item"
+          ><NuxtImg src="/images/youtube.svg"></NuxtImg
+        ></a>
+      </div>
     </div>
-  </template>
-  
-  <script setup>
-  const heightDevice = inject('devicePlatform')
-  </script>
-  
-  <style scoped lang="scss">
-  .about {
-    max-height: v-bind(heightDevice);
-    height: v-bind(heightDevice);
-    overflow-y: scroll;
-    &-wrapper {
-      height: calc(100dvh - 75px);
-      max-height: calc(100dvh - 75px);
-      padding: 0 20px;
+  </div>
+</template>
+
+<script setup>
+const heightDevice = inject('devicePlatform')
+</script>
+
+<style scoped lang="scss">
+.about {
+  max-height: v-bind(heightDevice);
+  height: v-bind(heightDevice);
+  overflow-y: scroll;
+  &-wrapper {
+    height: calc(100dvh - 75px);
+    max-height: calc(100dvh - 75px);
+    padding: 0 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    gap: 30px;
+  }
+  &-soc {
+    padding-bottom: 35px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 9px;
+    &-item {
+      width: 40px;
+      height: 40px;
+      background: #fff;
+      border-radius: 10px;
       display: flex;
-      flex-direction: column;
-      justify-content: space-between;
-      gap: 30px;
-    }
-    &-soc{
-        padding-bottom: 35px;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        gap: 9px;
-        &-item{
-            width: 40px;
-            height: 40px;
-            background: #fff;
-            border-radius: 10px;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            img{
-                width: 24px;
-                height: 24px;
-            }
-        }
-    }
-    &-logo{
-        margin-bottom: 20px;
-        img{
-            height: 50px;
-            max-width: 100%;
-        }
-    }
-    &-text{
-        p{
-            margin-bottom: 20px;
-            line-height: 16px;
-            font-size: 12px;
-            color: #2C2D35;
-            &:last-child{
-                margin-bottom: 0;
-            }
-        }
+      align-items: center;
+      justify-content: center;
+      img {
+        width: 24px;
+        height: 24px;
+      }
     }
   }
-  </style>
-  
+  &-logo {
+    margin-bottom: 20px;
+    img {
+      height: 50px;
+      max-width: 100%;
+    }
+  }
+  &-text {
+    p {
+      margin-bottom: 20px;
+      line-height: 16px;
+      font-size: 12px;
+      color: #2c2d35;
+      &:last-child {
+        margin-bottom: 0;
+      }
+    }
+  }
+}
+</style>
--- pages/profile/faq.vue
+++ pages/profile/faq.vue
@@ -1,101 +1,130 @@
 <template>
-    <div class="faq">
-      <UiHeaderComponent
-        center
-        center-text="Вопросы и ответы"
-        left-route="/profile"
-      ></UiHeaderComponent>
-      <div class="faq-wrapper">
-        <div class="faq-list">
-            <div class="faq-item">
-                <button class="faq-item-top collapsed" type="button" data-bs-toggle="collapse"  data-bs-target="#faq1" aria-expanded="false" aria-controls="faq1">
-                    <div class="faq-item-title">Куда попадут деньги от пожертвований?</div>
-                    <div class="faq-item-icon"><NuxtImg src="/images/tick.svg"></NuxtImg></div>
-                </button>
-                <div class="collapse faq-item-body" id="faq1">
-                    <div class="faq-item-content">Все денежные средства (пожертвования) через системы Payme, CLICK , UZUM и боксов попадают на расчетный счет фонда, и оттуда распределяются по программам.</div>
-                </div>
+  <div class="faq">
+    <UiHeaderComponent
+      center
+      center-text="Вопросы и ответы"
+      left-route="/profile"
+    ></UiHeaderComponent>
+    <div class="faq-wrapper">
+      <div class="faq-list">
+        <div class="faq-item">
+          <button
+            class="faq-item-top collapsed"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#faq1"
+            aria-expanded="false"
+            aria-controls="faq1"
+          >
+            <div class="faq-item-title">
+              Куда попадут деньги от пожертвований?
             </div>
-            <div class="faq-item">
-                <button class="faq-item-top collapsed" type="button" data-bs-toggle="collapse"  data-bs-target="#faq2" aria-expanded="false" aria-controls="faq2">
-                    <div class="faq-item-title">Куда попадут деньги от пожертвований?</div>
-                    <div class="faq-item-icon"><NuxtImg src="/images/tick.svg"></NuxtImg></div>
-                </button>
-                <div class="collapse faq-item-body" id="faq2">
-                    <div class="faq-item-content">Все денежные средства (пожертвования) через системы Payme, CLICK , UZUM и боксов попадают на расчетный счет фонда, и оттуда распределяются по программам.</div>
-                </div>
+            <div class="faq-item-icon">
+              <NuxtImg src="/images/tick.svg"></NuxtImg>
             </div>
+          </button>
+          <div id="faq1" class="collapse faq-item-body">
+            <div class="faq-item-content">
+              Все денежные средства (пожертвования) через системы Payme, CLICK ,
+              UZUM и боксов попадают на расчетный счет фонда, и оттуда
+              распределяются по программам.
+            </div>
+          </div>
+        </div>
+        <div class="faq-item">
+          <button
+            class="faq-item-top collapsed"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#faq2"
+            aria-expanded="false"
+            aria-controls="faq2"
+          >
+            <div class="faq-item-title">
+              Куда попадут деньги от пожертвований?
+            </div>
+            <div class="faq-item-icon">
+              <NuxtImg src="/images/tick.svg"></NuxtImg>
+            </div>
+          </button>
+          <div id="faq2" class="collapse faq-item-body">
+            <div class="faq-item-content">
+              Все денежные средства (пожертвования) через системы Payme, CLICK ,
+              UZUM и боксов попадают на расчетный счет фонда, и оттуда
+              распределяются по программам.
+            </div>
+          </div>
         </div>
       </div>
     </div>
-  </template>
-  
-  <script setup>
-  const heightDevice = inject('devicePlatform')
-  </script>
-  
-  <style scoped lang="scss">
-  .faq {
-    max-height: v-bind(heightDevice);
-    height: v-bind(heightDevice);
-    overflow-y: scroll;
-    &-wrapper {
-      height: calc(100dvh - 75px);
-      max-height: calc(100dvh - 75px);
-      padding: 0 20px;
+  </div>
+</template>
+
+<script setup>
+const heightDevice = inject('devicePlatform')
+</script>
+
+<style scoped lang="scss">
+.faq {
+  max-height: v-bind(heightDevice);
+  height: v-bind(heightDevice);
+  overflow-y: scroll;
+  &-wrapper {
+    height: calc(100dvh - 75px);
+    max-height: calc(100dvh - 75px);
+    padding: 0 20px;
+  }
+  &-item {
+    margin-bottom: 16px;
+    background: #fff;
+    border-radius: 6px;
+    padding: 10px 15px;
+    &:last-child {
+      margin-bottom: 0;
     }
-    &-item{
-        margin-bottom: 16px;
-        background: #fff;
-        border-radius: 6px;
-        padding: 10px 15px;
-        &:last-child{
-            margin-bottom: 0;
-        }
-        &-top{
-            background: none;
-            border: 0;
-            padding: 0;
-            display: flex;
-            align-items: center;
-            justify-content: space-between;
-            gap: 10px;
-            width: 100%;
-        }
-        &-top &-icon{
-            img{
-                transform: rotate(180deg);
-            }
-        }
-        &-top.collapsed &-icon{
-            img{
-                transform: rotate(0);
-            }
-        }
-        &-title{
-            width: calc(100% - 35px);
-            color: #363845;
-            font-size: 12px;
-            font-weight: 500;
-        }
-        &-icon{
-            width: 25px;
-            height: 25px;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            img{
-                width: 25px;
-            }    
-        }
-        &-content{
-            margin-top: 10px;
-            color: #6A6A6A;
-            font-size: 12px;
-            padding-top: 5px;
-            border-top: 1px solid #F6F6F6;
-        }
+    &-top {
+      background: none;
+      border: 0;
+      padding: 0;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      gap: 10px;
+      width: 100%;
+    }
+    &-top &-icon {
+      img {
+        transform: rotate(180deg);
+      }
+    }
+    &-top.collapsed &-icon {
+      img {
+        transform: rotate(0);
+      }
+    }
+    &-title {
+      width: calc(100% - 35px);
+      color: #363845;
+      font-size: 12px;
+      font-weight: 500;
+    }
+    &-icon {
+      width: 25px;
+      height: 25px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      img {
+        width: 25px;
+      }
+    }
+    &-content {
+      margin-top: 10px;
+      color: #6a6a6a;
+      font-size: 12px;
+      padding-top: 5px;
+      border-top: 1px solid #f6f6f6;
     }
   }
-  </style>
-  
+}
+</style>
