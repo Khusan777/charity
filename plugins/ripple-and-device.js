ecada08bb31f5dbdcd824a330e8a38c222924b0c
Хусан С. Абетов <abetov.x@ad.loc>
2024-03-13 10:54:33 +0500

    Refactor device platform condition

--- plugins/ripple-and-device.js
+++ plugins/ripple-and-device.js
@@ -3,7 +3,7 @@ import Ripple from 'vue3-whr-ripple-directive/src/ripple'
 export default defineNuxtPlugin((nuxtApp) => {
   const { isIos, isSafari, isMacOS, isApple } = useDevice()
   const deviceIsIOS = computed(() => {
-    return isIos || isSafari || isMacOS || isApple ? '100dvh' : '100vh'
+    return isIos || isSafari || isMacOS || isApple ? '100vh' : '100dvh'
   })
   nuxtApp.vueApp.provide('devicePlatform', deviceIsIOS.value)
   nuxtApp.vueApp.directive('ripple', Ripple)
