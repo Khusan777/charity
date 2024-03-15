190aba542a74ec830804d3a65765f99ed7cdb97f
Хусан С. Абетов <abetov.x@ad.loc>
2024-03-15 14:10:51 +0500

    ADD userscalable metateg

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,6 +19,8 @@ export default defineNuxtConfig({
   app: {
     pageTransition: { name: 'page', mode: 'out-in', appear: true },
     head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width,initial-scale=1,user-scalable=no',
       script: [
         {
           src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js',
@@ -26,6 +28,16 @@ export default defineNuxtConfig({
             'sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2',
           crossorigin: 'anonymous',
           defer: true,
+        },
+      ],
+      meta: [
+        {
+          'http-equiv': 'X-UA-Compatible',
+          content: 'IE=edge',
+        },
+        {
+          name: 'viewport',
+          content: 'width=device-width,initial-scale=1,user-scalable=no',
         },
       ],
     },
