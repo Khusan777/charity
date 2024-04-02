3207b5ce4cc6d4b0592c9a5ef7f28c4de5d77297
Хусан С. Абетов <abetov.x@ad.loc>
2024-04-02 10:42:10 +0500

    REFACTOR: Remove config env

--- components/ChartCardNotCollected.vue
+++ components/ChartCardNotCollected.vue
@@ -1,9 +1,6 @@
 <template>
   <div class="chart-card">
     <div @click.stop="goToDetailPage(feeItem?.id)">
-      <p style="color: #ffffff">
-        {{ config.public?.apiBase + '/storage/' + feeItem?.fond?.icon }}
-      </p>
       <div class="fond-container">
         <div class="data">
           <NuxtImg
@@ -101,7 +98,6 @@
 </template>
 
 <script setup>
-const config = useRuntimeConfig()
 const appStore = useAppStore()
 const router = useRouter()
 const props = defineProps({
