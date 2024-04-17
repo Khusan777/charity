acb2951f312ad160dd553f5aaccef16d4dbd8b5b
Хусан С. Абетов <abetov.x@ad.loc>
2024-04-17 23:16:01 +0500

    REFACTOR: Infinite scroll in news tabs

--- nuxt.config.js
+++ nuxt.config.js
@@ -95,4 +95,5 @@ export default defineNuxtConfig({
   //   fallback: 'light',
   // },
   css: [{ src: '~/assets/css/bootstrap.min.css', lang: 'css' }],
+  link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
 })
