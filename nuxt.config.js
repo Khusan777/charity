9bf92b2de92c52f473a5df4bd2515cad40353088
Хусан С. Абетов <abetov.x@ad.loc>
2024-04-19 16:55:44 +0500

    Add env vsriable for paid url

--- nuxt.config.js
+++ nuxt.config.js
@@ -68,6 +68,7 @@ export default defineNuxtConfig({
   runtimeConfig: {
     public: {
       apiBase: process.env.NUXT_PUBLIC_API_BASE,
+      urlPay: process.env.NUXT_PUBLIC_URL_PAY,
     },
   },
   imports: {
