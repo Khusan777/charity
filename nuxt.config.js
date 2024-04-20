c27a450426a4fc15f29bba688a7dba00f28eff28
Хусан С. Абетов <abetov.x@ad.loc>
2024-04-20 07:40:10 +0500

    FEATURE: Add urlHost env variable

--- nuxt.config.js
+++ nuxt.config.js
@@ -69,6 +69,7 @@ export default defineNuxtConfig({
     public: {
       apiBase: process.env.NUXT_PUBLIC_API_BASE,
       urlPay: process.env.NUXT_PUBLIC_URL_PAY,
+      urlHost: process.env.NUXT_PUBLIC_HOST,
     },
   },
   imports: {
